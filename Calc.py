892b55048c92ba3f5bf62cf713979d7fe09dd632
Psychokater <93474242+Psychokater@users.noreply.github.com>
2022-07-22 07:31:31 +0200

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -4,148 +4,91 @@
 # Date:   20.07.2022        #                         
 #############################
 
+import Operations
 
-### Methode wird nur einmal am Anfang ausgeführt, erste Zahl wird gegeben.
+### Methode wird nur einmal am Anfang ausgeführt, erste Zahl wird vom User verlangt.
 def start():
     firstNumber = 0
-    userInput = input("Sie können das Programm jederzeit mit \"Exit\" beenden.\nBitte geben Sie die erste Zahl ein: ")
+    userInput = input("Sie können das Programm jederzeit mit \"Exit\" beenden.\nBitte geben Sie die erste Zahl ein:\t\t\t ")
+    if userInput.lower() == "exit":
+        exit()
+    else:  
+        try:
+            firstNumber = int(userInput)    
+                       
+            return firstNumber
+        except:
+            print("\nEingabe ungültig!\n")
+            start()
+
+### Methode zur Anforderung der ersten Zahl vom User ////////// Kommt nach firstRepeater() wenn "neu" gewählt wurde!
+def userInputFirstNumber():
+    userInput = input("Bitte geben Sie die erste Zahl ein:\t\t\t ")
     if userInput.lower() == "exit":
         exit()
     else:
-        firstNumber = int(userInput)
+        try:
+            firstNumber = int(userInput)
 
-    return firstNumber
+            return firstNumber
+        except:
+            print("\Eingabe ungültig!\n")
+            start()    
 
-### Methode für die Lösung, Aufforderung zum Weiter/Erneut-Rechnen oder Beenden    
-def firstRepeater(solution):
-    userInput = input("Das Ergebnis ist: {0} \nMöchten Sie mit dem Ergebnis weiterrechnen, geben Sie ein: \"Weiter\"\nFür eine erneute Rechnung geben Sie ein: \"Neu\"\nZum Beenden geben Sie ein: \"Exit\"\nEingabe: ".format(solution)).lower()
+### Methode für die Lösung, Aufforderung User zum Weiter/Erneut-Rechnen oder Beenden    
+def Repeater(solution2):
+    userInput = input("Das Ergebnis ist:\t\t\t\t\t {0} \nMit dem Ergebnis weiterrechnen: \"Weiter\"\nErneute Rechnung:"
+    " \"Neu\"\nBeenden: \"Exit\"\nEingabe:\t\t\t\t\t\t ".format(solution2)).lower()
 
     match userInput:
-        case "weiter": 
-            nextCalcProg(solution)
+        case "weiter":
+            nextCalcProg(solution2)
         case "neu":
             newCalcProg()
         case "exit":
             exit()
         case _:
-            print("Eingabe ungültig!")
-            firstRepeater(solution)
+            print("\nEingabe ungültig!")
+            Repeater(solution2)
 
-### Methode für die Lösung, Aufforderung zum Weiter/Erneut-Rechnen, Rechnen vom letzten Ergebnis oder Beenden.
-def secondRepeater(solution):
-    userInput = input("Das Ergebnis ist: {0} \nMöchten Sie mit dem Ergebnis weiterrechnen, geben Sie ein: \"Weiter\"\nFür eine erneute Rechnung geben Sie ein: \"Neu\"\n"
-    "Um die letzte Rechnung rückgängig zu machen, geben Sie ein: \"Fehler\"\nZum Beenden geben Sie ein: \"Exit\"\nEingabe: ".format(solution)).lower()
-
-    match userInput:
-        case "weiter": 
-            nextCalcProg(solution)
-        case "neu":
-            newCalcProg()
-        case "exit":
-            exit()
-        case "fehler":
-            wrongCalcProg()
-        case _:
-            print("Eingabe ungültig!")
-            firstRepeater(solution)
-
-### Methode zur Anforderung der ersten Zahl ////////// Kommt nach firstRepeater() wenn "neu" gewählt wurde!
-def getFirstNumber():
-    userInput = input("Bitte geben Sie die erste Zahl ein: ")
-    if userInput.lower() == "exit":
-        exit()
-    else:
-        firstNumber = int(userInput)
-
-    return firstNumber
-
-### Methode kommt IMMER zur Anforderung der zweiten Zahl //////// Kommt nach firstRepeater() wenn "weiter" gewählt wurde!
-def getNextNumber():
-    userInput = input("Bitte geben Sie eine weitere Zahl ein: ")
-    if userInput.lower() == "exit":
-        exit()
-    else:
-        secondNumber = int(userInput)
-
-    return secondNumber
-
-### Methode zur Auswahl des Operators
-def getOperator(firstNumber,secondNumber):
-    userInput = input("Bitte geben Sie den Rechenoperator ein (+,-,*,/): ")
+### Methode zur Auswahl des Operators durch User
+def userInputOperator(firstNumber):
+    userInput = input("Bitte geben Sie den Rechenoperator ein (+,-,*,/):\t ")
     if userInput.lower() == "exit":
         exit()
     else:
         match userInput:
             case "+":
-                solution = calcAdd(firstNumber,secondNumber)
+                solution = Operations.calcAdd(firstNumber)
             case "-":
-                solution = calcSub(firstNumber,secondNumber)
+                solution = Operations.calcSub(firstNumber)
             case "*":
-                solution = calcMul(firstNumber,secondNumber)
+                solution = Operations.calcMul(firstNumber)
             case "/":
-                solution = calcDiv(firstNumber,secondNumber)
+                solution = Operations.calcDiv(firstNumber)
             case _:
-                print("Eingabe ist kein gültiges Zeichen!")
-                getOperator(firstNumber,secondNumber)
+                print("\nEingabe ist kein gültiges Zeichen!\n")
+                userInputOperator(firstNumber)
     
-    return solution
-
-### Variable zur Speicherung des letzten Ergebnisses
-def getBackupNumber(saveNumber):
-    backupNumber = saveNumber
-
-    return backupNumber
-
-def calcAdd(a, b):
-    solution = a + b
-
-    return solution
-
-def calcSub(a, b):
-    solution = a - b
-
-    return solution
-
-def calcMul(a, b):
-    solution = a * b
-
-    return solution
-    
-def calcDiv(a, b):
-    solution = a / b
-
     return solution
 
 ### Funktion wird als erstes aufgerufen für die erste Berechnung
-def firstCalcProg():
-    firstNumber = start()
-    secondNumber = getNextNumber()
-    solution = getOperator(firstNumber,secondNumber)
-    getBackupNumber(solution)
-    firstRepeater(solution)
+def Main():
+    firstNumber = start()    
+    solution2 = userInputOperator(firstNumber)    
+    Repeater(solution2)
 
 ### Funktion wird aufgerufen für eine "neue Berechnung"
 def newCalcProg():
-    firstNumber = getFirstNumber()
-    secondNumber = getNextNumber()
-    solution = getOperator(firstNumber,secondNumber)
-    getBackupNumber(solution)
-    firstRepeater(solution)
+    firstNumber = userInputFirstNumber()    
+    solution2 = userInputOperator(firstNumber)    
+    Repeater(solution2)
 
 ### Funktion wird aufgerufen für eine "folge-Berechnung"
 def nextCalcProg(solution):
-    firstNumber = solution
-    secondNumer = getNextNumber()
-    solution = getOperator(firstNumber,secondNumer)
-    getBackupNumber(solution)
-    secondRepeater(solution)
+    firstNumber = solution    
+    solution2 = userInputOperator(firstNumber)    
+    Repeater(solution2)
 
-### Funktion wird aufgerufen um auf das letzte Ergebnis zurückzukommen.
-def wrongCalcProg():
-    firstNumber = getBackupNumber()
-    secondNumber = getNextNumber()
-    solution = getOperator(firstNumber,secondNumber)
-    getBackupNumber(solution)
-    firstRepeater(solution)
+Main()
 
-firstCalcProg()
